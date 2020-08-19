7eb9e41aabd03a89211ec56a929e99a2037fb9cb
Matt Herriges <herrigesmt@gmail.com>
2020-08-18 23:46:45 -0700

    code challenge 7

--- __tests__/test-array-binary-search.js
+++ __tests__/test-array-binary-search.js
@@ -7,15 +7,15 @@ describe('array-binary-search test', () => {
         let array = [11, 22, 33, 44, 55, 66, 77, 88 , 99, 111];
         let val = 44;
         expect(binSearch(array, val)).toBe(3);
-    })
+    });
     it('should return number of index in sorted array if in second half of array', () => {
         let array = [11, 22, 33, 44, 55, 66, 77, 88 , 99, 111];
         let val = 77;
         expect(binSearch(array, val)).toBe(6);
-    })
+    });
     it('should return -1 if val is not in sorted array', () => {
         let array = [11, 22, 33, 44, 55, 66, 77, 88 , 99, 111];
         let val = 49;
-        expect(binSearch(array, val)).toBe(-1);    
-    })
-})
+        expect(binSearch(array, val)).toBe(-1);
+    });
+});
