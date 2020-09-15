c2d2ae49c9cec2436ecbe3672e9c0aca086fa87b
Matt Herriges <herrigesmt@gmail.com>
2020-09-14 23:04:03 -0700

    added write-up, code, tests for insertion sort

---
+++ sorts/insertionSort/insertion-sort.js
@@ -0,0 +1,12 @@
+module.exports = array => {
+    for(let i = 1; i < array.length; i++){
+        let j = i - 1;
+        let temp = array[i];
+        while(j >= 0 && temp < array[j]){
+            array[j+1] = array[j];
+            j--;
+        }
+        array[j + 1] = temp;
+    }
+};
+
