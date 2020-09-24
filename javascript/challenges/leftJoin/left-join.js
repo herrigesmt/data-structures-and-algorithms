04271f08590f8183e9a45b60b52614e7b3bb18cc
Matt Herriges <herrigesmt@gmail.com>
2020-09-23 18:05:11 -0700

    left-join

---
+++ javascript/challenges/leftJoin/left-join.js
@@ -0,0 +1,11 @@
+function leftJoin(leftHash, rightHash){
+    if(!(rightHash && leftHash)) {throw new Error('No values in hash table(s)');}
+    let outputArray = [];
+    for(let key in leftHash){
+        let keyArray = [key, leftHash[key], (rightHash[key] || null)];
+        outputArray.push(keyArray);
+    }
+    return outputArray;
+}
+
+module.exports = leftJoin;
